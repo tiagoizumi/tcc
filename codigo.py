fa12b0a13a81438664f1b232bc273455810a5f1d
tiago izumi <tiagoizumi@gmail.com>
2026-05-30 22:07:39 -0300

    run 3

--- codigo.py
+++ codigo.py
@@ -1,4 +1,7 @@
+# python codigo.py > Program/runAll.sh
+print("#arquivo_de_lote")
+print("#!/bin/bash")
 for i in range(1, 11):
-    for j in [20, 25, 30, 40]:
-        for k in [25, 50, 75, 100]:
-            print(f'./runTest "../Instances/CKP_Classical_Instances/{k}/{i}_{j}_{k}" 5')
+    for n in [20, 25, 30, 40, 50, 60, 70]:
+        for d in [25, 50, 75, 100]:
+            print(f'./runTest "../Instances/CKP_Classical_Instances/{d}/{i}_{n}_{d}" 5')
